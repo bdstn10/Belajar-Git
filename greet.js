bb6d9a7cd594fd2af8fe946f1d538c1f68b978fa
Budi Setiawan <budistn1085@gmail.com>
2024-02-02 21:05:07 +0700

    Update test git diff

--- greet.js
+++ greet.js
@@ -1,3 +1,5 @@
 for (let i = 0; i < 10; i++) {
-    console.log("Ini adalah update pada branch testing")   
+    console.log("Ini adalah update kedua pada branch testing")   
 }
+
+// Test change git
